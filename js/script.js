21f6cca6cd07349ccec0cf0a628a4694f20f1d83
Vladimir <torna7@yandex.ru>
2020-11-11 14:50:22 +0300

    For Github Pages

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('.reviews__carousel').slick({
+        autoplay: false,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>'
+    });
+  });
+
+  window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.header__menu'),
+    menuItem = document.querySelectorAll('.header__item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('header__menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('header__menu_active');
+        })
+    })
+})
